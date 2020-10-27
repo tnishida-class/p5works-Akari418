3ec683a42be068ab15287db085def162a93d82f9
Akari418 <akariymnk.81402@outlook.jp>
2020-10-27 10:53:33 +0900

    changeq2

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,13 @@
 // 小手調べ
 function setup(){
+  noFill ();
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
-  }
+ellipse (50,50,10*(i+1));
+if(i < 4){
+    stroke(0,0,255);
+}
+else{
+  stroke (255,0,0);
+}
+}
 }
