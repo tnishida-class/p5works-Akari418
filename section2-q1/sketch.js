786fd6e812d6a59dc9ffeb92a683c0240ddb68b4
Akari418 <akariymnk.81402@outlook.jp>
2020-10-27 11:07:50 +0900

    change2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -2,8 +2,9 @@
 function setup(){
   noFill ();
   for(let i = 0; i < 10; i++){
-ellipse (50,50,10*(i+1));
-if(i < 4){
+    let j = i * 10 + 10;
+ellipse (50,50,j,j);
+if(i < 5){
     stroke(0,0,255);
 }
 else{
