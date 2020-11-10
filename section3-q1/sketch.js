2ce4fbaf5a305b0cca92cf3759cd451bdbcdd59b
Akari418 <akariymnk.81402@outlook.jp>
2020-11-10 12:12:09 +0900

    change3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -3,16 +3,16 @@
 // 吹き出しにしっぽを付けてみよう
 function setup(){
   createCanvas(400, 400);
-  background(255);
-  balloon("I love keyakizaka46");
+  background(255)
+  balloon("I love keyakizaka46", 50, 200, "#003462");
 }
 
-function balloon(t){
+function balloon(t, a, b, c){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
-  let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
+  let p = 50;
+  fill(c);
+  ellipse(110 + a, 65 + b, w + p * 2, h + p * 2);
   fill(255);
-  text(t, p, h + p);
+  text(t, p + a, h + p + b);
 }
