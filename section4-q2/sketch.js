67519dc77e6100f88af3c69c2aec6121dbcdb956
Akari418 <akariymnk.81402@outlook.jp>
2020-11-23 19:58:06 +0900

    change4-q2

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -1,18 +1,22 @@
 // テキスト「アニメーション」
 let x, y, vx, vy;
+let angle = 0;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
   x = width / 2;
   y = height / 2;
-  vx = 8;
-  vy = 8;
+  vx = 6;
+  vy = 6;
 }
 
 function draw(){
   background(160, 192, 255);
   // BLANK[2] (hint: 作った star 関数を使います)
-
+angle +=0.1;
+x += vx;
+y +=vy;
+star (x, y, 30, angle);
   // 端の処理パターン (1) 反対側から出てくる
   if(x > width){ x = 0; }
   else if(x < 0){ x = width; }
@@ -21,10 +25,14 @@ function draw(){
 }
 
 function star(cx, cy, r, angle){
+  push();
+  noStroke();
+  fill(250, 221, 0);
   beginShape();
   for(var i = 0; i < 20; i++){
     var theta = TWO_PI * i * 2 / 5 - HALF_PI;
     // BLANK[1] (hint: angle 分だけ星を回転させるには？)
+theta = theta + angle;
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
     vertex(x,y);
