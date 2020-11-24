4061694e378cdd2f7fd6da72f6e6e026629f187c
Akari418 <akariymnk.81402@outlook.jp>
2020-11-24 09:34:34 +0900

    change finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,13 +1,29 @@
-// 最終課題を制作しよう
-
-function setup(){
+let n = 10;
+let snows = new Array(100);
+let v = 2.5;
+function setup() {
   createCanvas(windowWidth, windowHeight);
-}
+  for(i = 0; i < snows.length; i++){
+    snows[i] = [random(width), random(height), random(5,16)];
+  }
 
-function draw(){
-  background(160, 192, 255);
 }
-
 function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth, windowHeight)}
+
+function draw() {
+  background(0)
+  noStroke();
+  for(j = 0; j < snows.length; j++){
+    drawLight(snows[j]);
+    snows[j][1] += v;
+  }
+
+}
+
+function drawLight(snow){
+  for(i = 0;i < n;i++){
+    fill(255, 255/n);
+    ellipse(snow[0], (snow[1])%height, (snow[2] / n)*(n - i));
+  }
 }
