45e67f19d4c6e69bfc09f8dac669bb0e00e61dd6
Akari418 <akariymnk.81402@outlook.jp>
2021-01-01 17:29:27 +0900

    Update sketch.js
    change 3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,15 +4,15 @@
 function setup(){
   createCanvas(400, 400);
   background(255)
-  balloon("I love keyakizaka46", 50, 200, "#003462");
+  balloon("I love keyakizaka46", 10, 50, 25 ,500);
 }
 
-function balloon(t, a, b, c){
+function balloon(t, x, y, bc ,tc){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
-  let p = 50;
-  fill(c);
-  ellipse(110 + a, 65 + b, w + p * 2, h + p * 2);
-  fill(255);
-  text(t, p + a, h + p + b);
+  let p = 30;
+  fill(bc);
+  rect(x, y, w + p * 2, h + p * 2);
+  fill(tc);
+  text(t, p + x, h + p + y);
 }
